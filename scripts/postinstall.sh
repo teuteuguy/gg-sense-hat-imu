d8e7c5d0401eff4f07b2c4c8b1c68e2ec326ca8e
Tim <pub@lalatte.com>
2018-02-19 23:27:47 +0800

    Last test

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -20,8 +20,8 @@ echo $IOT_CREDENTIAL_ENDPOINT
 echo $IOT_GG_GROUP_NAME
 echo $AWS_ACCESS_KEY_ID, $AWS_SECRET_ACCESS_KEY, $AWS_SESSION_TOKEN
 
-LAMBDA_FUNCTION_ARN=`$AWS_COMMAND resourcegroupstaggingapi get-resources --tag-filters Key=gg-dev-pipeline,Key=type,Values=lambda --query ResourceTagMappingList[].ResourceARN --output text`
-LAMBDA_FUNCTION_NAME=`$AWS_COMMAND lambda list-functions --query 'Functions[?FunctionArn==\`$LAMBDA_FUNCTION_ARN\`].FunctionName' --output text`
+LAMBDA_FUNCTION_ARN=`$AWS_COMMAND resourcegroupstaggingapi get-resources --region $AWS_REGION --tag-filters Key=gg-dev-pipeline,Key=type,Values=lambda --query ResourceTagMappingList[].ResourceARN --output text`
+LAMBDA_FUNCTION_NAME=`$AWS_COMMAND lambda list-functions --region $AWS_REGION --query 'Functions[?FunctionArn==\`$LAMBDA_FUNCTION_ARN\`].FunctionName' --output text`
 LAMBDA_ALIAS_NAME="gg-dev-pipeline"
 
 echo "Zipping..."
@@ -30,17 +30,17 @@ rm -f package.zip
 zip -rq package.zip *
 
 echo "Uploading to Lambda"
-$AWS_COMMAND lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://`pwd`/package.zip
+$AWS_COMMAND lambda update-function-code --region $AWS_REGION --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://`pwd`/package.zip
 echo "Publishing new Lambda version"
-LAMBDA_PUBLISH_VERSION=`$AWS_COMMAND lambda publish-version --function-name $LAMBDA_FUNCTION_NAME --query Version --output text`
+LAMBDA_PUBLISH_VERSION=`$AWS_COMMAND lambda publish-version --region $AWS_REGION --function-name $LAMBDA_FUNCTION_NAME --query Version --output text`
 echo "Updating the alias"
-$AWS_COMMAND  lambda update-alias --name $LAMBDA_ALIAS_NAME --function-name $LAMBDA_FUNCTION_NAME --function-version $LAMBDA_PUBLISH_VERSION
+$AWS_COMMAND  lambda update-alias --region $AWS_REGION --name $LAMBDA_ALIAS_NAME --function-name $LAMBDA_FUNCTION_NAME --function-version $LAMBDA_PUBLISH_VERSION
 
-GROUP_ID=`$AWS_COMMAND greengrass list-groups --query "Groups[?Name=='$IOT_GG_GROUP_NAME'].Id" --output text`
-GROUP_VERSION_ID=`$AWS_COMMAND greengrass list-groups --query "Groups[?Name=='$IOT_GG_GROUP_NAME'].LatestVersion" --output text`
+GROUP_ID=`$AWS_COMMAND greengrass list-groups --region $AWS_REGION --query "Groups[?Name=='$IOT_GG_GROUP_NAME'].Id" --output text`
+GROUP_VERSION_ID=`$AWS_COMMAND greengrass list-groups --region $AWS_REGION --query "Groups[?Name=='$IOT_GG_GROUP_NAME'].LatestVersion" --output text`
 
 echo "Deploying to GG"
-$AWS_COMMAND greengrass create-deployment --group-id $GROUP_ID --deployment-type NewDeployment --group-version-id $GROUP_VERSION_ID
+$AWS_COMMAND greengrass create-deployment --region $AWS_REGION --group-id $GROUP_ID --deployment-type NewDeployment --group-version-id $GROUP_VERSION_ID
 
 
 
